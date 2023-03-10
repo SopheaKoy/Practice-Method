0276661e1b2cbaea19bb891f92f87a89c2ff68ec
SopheaKoy <koysophea09@gamil.com>
2023-03-10 23:40:37 +0700

    Push

---
+++ src/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+    int id;
+    int age;
+    String name;
+
+    //  Default Constructor
+    Person(){
+        id=1001;
+        age=21;
+        name=" Koy Sophea";
+    }
+}
